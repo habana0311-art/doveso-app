a10ff648c0eacc194ea39050f952aa6ae97f89aa
habana0311-art <habana0311@gmail.com>
2026-09-21 22:32:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def extract_from_image(image):
     ]
     """
     res = client.models.generate_content(
-        model='gemini-2.5-flash',
+        model='gemini-3.6-flash',
         contents=[image, prompt],
         config=types.GenerateContentConfig(response_mime_type="application/json")
     )
